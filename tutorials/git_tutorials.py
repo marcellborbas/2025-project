af38568ba39c74540968df5bbfdfb1d047fd6516
Borbas Marcell <borbas.marcell@freemail.hu>
2025-03-06 19:26:10 +0100

    git basic flow tutorial

---
+++ tutorials/git_tutorials.py
@@ -0,0 +1,12 @@
+"""
+Git -
+
+A git egy verzió kezelő rendszer.
+A kód váltásainak kezelését oldja meg, illetve amikor egy feladaton
+egyszerre többen dolgozunk, ahhoz tud nagyon sok segítséget adni.
+
+Untracked állapot - Az olyan fájlok amellyeket a GIT még nem követ (nincsenek a verzió követésben)
+Staged állapot - Olyan fájlok amellyeket hozzá adtál a staging areához  / egyfajta mentési pont
+Commited állapot - A változtatások elmentésre kerülnek a helyi repositoryban 
+
+"""
